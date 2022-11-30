bc2d8adf4d9d6c6bfd647469d6a511e85c478e47
Liza Carvelli <liza@liza.sh>
2022-11-30 14:17:11 +0100

    Update server build script

--- Pal.Server/build.sh
+++ Pal.Server/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o artifacts
+dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o ~/server
